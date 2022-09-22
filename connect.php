7bcfa098a854a1833aa58da8e016a61711fbcc5d
LauriHAMK <lauri.mi.jokinen@student.hamk.fi>
2022-09-22 10:21:10 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 
 mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
 try{
-    $yhteys=mysqli_connect("localhost:3306", "root", "", "cool_test");
+    $con=mysqli_connect("localhost:3306", "root", "", "cool_test");
 }
 catch(Exception $e){
     print("Error");
