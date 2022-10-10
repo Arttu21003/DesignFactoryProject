9ca3fb9b532910344e313c874e1fe9be6be35502
Arttu21003 <arttu.altio@student.hamk.fi>
2022-10-10 13:25:49 +0300

    sheisse

---
+++ SignUpForm/login.php
@@ -0,0 +1,54 @@
+<?php
+
+session_start();
+$json=isset($_POST["user"]) ? $_POST["user"] : "";
+
+if (!($user=tarkistaJson($json))){
+    print "Fill all fields";
+    exit;
+}
+
+mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
+// mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+try{
+    $yhteys=mysqli_connect("localhost", "root", "", "wp_TRTKP21A3_1");
+}
+catch(Exception $e){
+    print "Yhteysvirhe";
+    exit;
+}
+
+$sql="select * from users where tunnus=? and salasana=SHA2(?, 256)";
+try{
+    $stmt=mysqli_prepare($yhteys, $sql);
+    mysqli_stmt_bind_param($stmt, 'ss', $user->tunnus, $user->salasana);
+    mysqli_stmt_execute($stmt);
+    $tulos=mysqli_stmt_get_result($stmt);
+    if ($rivi=mysqli_fetch_object($tulos)){
+        $_SESSION["kayttaja"]="$rivi->tunnus";
+        print "ok";
+        exit;
+    }
+    mysqli_close($yhteys);
+    print $json;
+}
+catch(Exception $e){
+    print "Jokin virhe!";
+}
+?>
+
+
+<?php
+function tarkistaJson($json){
+    if (empty($json)){
+        return false;
+    }
+    $user=json_decode($json, false);
+    if (empty($user->tunnus) || empty($user->salasana)){
+        return false;
+    }
+    return $user;
+}
+
+?>
